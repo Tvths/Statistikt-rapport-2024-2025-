76d646d83e7a964c189a124ab171326d84705996
Viet tien Trinh <timcenter0312@gmail.com>
2024-09-25 01:47:27 +0200

    Update OSU_urval.R

--- OSU_urval.R
+++ OSU_urval.R
@@ -3,9 +3,9 @@
 sample_for_OSU_Urval <- sample(index(data),size=30,replace=FALSE)
 OSU_Urval <- data[sample_for_OSU_Urval,]
 
-OSU_Urval1 <- OSU_Urval[c("Kommunkod","Region","Medelålder")]
+OSU_Urval1 <- OSU_Urval[c("Kommunkod","Region","Population")]
 
-OSU_Urval2 <- OSU_Urval[c("Kommunkod","Region","Dödfall")]
+OSU_Urval2 <- OSU_Urval[c("Kommunkod","Region","Andel_födda_på_population")]
 
 
 
